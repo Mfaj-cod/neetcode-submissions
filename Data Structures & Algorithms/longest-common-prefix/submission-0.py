5a5b721502c3dcb34bd8a36eeea111feca41ca62
M. Fajal <fajalmoaj@gmail.com>
2026-05-02 10:45:13 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        lcp = ""
+        c = 0
+        for i in range(len(strs)):
+            for j in range(i+1, len(strs)):
+                c = 0
+                while c < len(strs[i]):
+                    if strs[i][c] != strs[j][c]:
+                        return lcp
+                    lcp += strs[i][c]
+                    c += 1
+        
+        return lcp
