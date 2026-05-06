7caae9daed44099d66e2b12ad2fc9482665051b0
M. Fajal <fajalmoaj@gmail.com>
2026-05-06 17:21:26 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        l, r = 0, x//2
+        ans = 1
+
+        while l <= r:
+            m = l + (r - l) // 2
+            m_sq = m * m
+            if m_sq == x:
+                return m
+            elif m_sq > x:
+                r = m - 1
+            else:
+                ans = m
+                l = m + 1
+        
+        return ans
+            
