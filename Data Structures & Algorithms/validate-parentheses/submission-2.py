783da83f59cc164002b48d165732b34dd8dbc72a
M. Fajal <fajalmoaj@gmail.com>
2026-05-06 12:31:50 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = { ')': '(', '}': '{', ']': '[' }
+        stk = []
+
+        for c in s:
+            if c in hashmap:
+                if stk and stk[-1] == hashmap[c]:
+                    stk.pop()
+                else:
+                    return False
+            else:
+                stk.append(c)
+
+        return True if not stk else False
